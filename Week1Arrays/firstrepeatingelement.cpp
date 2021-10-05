753d3876e71edcd40e8f3295d46ef7eefbbc7ca5
Mehak Aggarwal <59011127+mehak-aggarwal9300@users.noreply.github.com>
2021-10-05 15:52:06 +0530

    Update firstrepeatingelement.cpp

--- Week1Arrays/firstrepeatingelement.cpp
+++ Week1Arrays/firstrepeatingelement.cpp
@@ -1,11 +1,19 @@
 //https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1/?category%5B%5D=Arrays&category%5B%5D=Arrays&difficulty%5B%5D=0&page=2&query=category%5B%5DArraysdifficulty%5B%5D0page2category%5B%5DArrays
 
 //optimize...
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        
+        for(int i=0;i<n;i++)
         {
-            if(arr[i]==arr[j]) return i+1; 
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j]) return i+1; 
+            }
         }
+        return -1;
     }
-    return -1;
+};
